4eb3c24c0e8f390846dd5dcd5798e2f003ae7836
BirNataly <141635890+BirNataly@users.noreply.github.com>
2023-09-11 09:45:07 +0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -2,5 +2,4 @@
 select count (o."inDelivery") as "count_inDelivery", c.login from "Orders" as o inner join "Couriers" as c on c.id=o."courierId" group by c.login;
 
 # Задание 2
-
 select track, case when finished=true then 2 when cancelled=true then -1 when "inDelivery"=true then 1 else 0 end as status from "Orders";
